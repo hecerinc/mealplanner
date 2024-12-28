bc17370519372a1f9be5e3950c2da33ad6e39d58
Hector Rincon <hecerinc@hotmail.com>
2024-12-27 22:49:08 -0600

    Add server

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 import json
 from flask_cors import CORS
 import logging
@@ -34,4 +34,26 @@ def save_collection(collection):
 	with open(db, 'w', encoding="utf-8", newline='\n') as fd:
 		json.dump(c, fd, indent=4)
 	return jsonify({'success': 'success', 'collection': collection})
+
+
+@app.route("/api/week_menu", methods=['GET', 'POST'])
+def week_menu():
+	if request.method == 'GET':
+		'''
+		For this method, we will get the initial Sunday of the week in YYYY-MM-DD format from the qs
+		'''
+		week = request.args.get('week')
+		if week is None:
+			abort(400)
+		return fetch_week_menu(week)
+	else:
+		return save_week_menu()
+
+def fetch_week_menu(week: str):
+	week_from_db = {}
+	return jsonify(week_from_db)
+	
+def save_week_menu():
+	menu = {}
+	return jsonify({'success': 'success', 'menu': menu})
 
