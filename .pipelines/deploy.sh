614c90b38c8d565856005e2e25762bf687d642bd
Hector Rincon <hecerinc@hotmail.com>
2025-01-28 20:39:40 -0600

    Add deployment pipeline

---
+++ .pipelines/deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# UI
+
+## Build react
+tz=$(date +"%Y-%m-%d %H:%M:%S%z")
+echo "$tz Deploying mealplanner."
+echo "-------------------------------------------------"
+
+DEST='/var/www/html/atom'
+cd ui/
+if [ "$ENVIRONMENT" == "PRODUCTION" ]; then
+	rm -rf build
+	PUBLIC_URL='/atom' yarn build
+	mv build/* $DEST
+fi
+
+## Move to atom folder
+
+
+# API
+
+## Restart the service
+sudo systemctl restart mealplanner_api
+
+tz=$(date +"%Y-%m-%d %H:%M:%S%z")
+echo "-------------------------------------------------"
+echo "$tz Deployment done."
